a8066d04f7286565486f5fbc1742bfbb1e663dbe
LukasKubicek <lukaskubicek@gmail.com>
2022-09-13 08:44:10 +0200

    vadiace - klient a server, datove anotace

--- MVCIS/MVCIS/Controllers/PersonController.cs
+++ MVCIS/MVCIS/Controllers/PersonController.cs
@@ -40,6 +40,7 @@ namespace MVCIS.Controllers
 
         public IActionResult Edit(int id)
         {
+           
             var person = PersonDataset.People.FirstOrDefault(x => x.Id == id);
 
             if(person == null)
@@ -63,6 +64,10 @@ namespace MVCIS.Controllers
         [HttpPost]
         public IActionResult EditPerson(Person person)
         {
+            if (!ModelState.IsValid)
+            {
+                return View("Edit", person);
+            }
             var existing = PersonDataset.People.FirstOrDefault(x => x.Id == person.Id);
 
             if (existing == null)
@@ -73,6 +78,7 @@ namespace MVCIS.Controllers
             existing.FirstName = person.FirstName;
             existing.LastName = person.LastName;
             existing.DateOfBirth = person.DateOfBirth;
+            existing.Email = person.Email;
 
             return RedirectToAction("Detail", new { id = existing.Id });
         }
--- MVCIS/MVCIS/Models/Person.cs
+++ MVCIS/MVCIS/Models/Person.cs
@@ -1,11 +1,25 @@
-﻿namespace MVCIS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCIS.Models
 {
     public class Person
     {
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Jméno")]
         public string? FirstName {get; set; }
+        
+        [MaxLength(10)]
+        [Display(Name = "Příjmení")]
+
         public string? LastName {get; set; }
+        
+        [EmailAddress]
         public string? Email {get; set; }
+
+        [Display(Name = "Datum narození")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
     }
 }
