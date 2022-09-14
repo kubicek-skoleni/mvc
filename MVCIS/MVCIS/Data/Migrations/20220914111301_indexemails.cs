add994de16989a8ce69a56dc9322daf255f379e4
LukasKubicek <lukaskubicek@gmail.com>
2022-09-14 13:16:17 +0200

    index na fulltextove hledani

---
+++ MVCIS/MVCIS/Data/Migrations/20220914111301_indexemails.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCIS.Data.Migrations
+{
+    public partial class indexemails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Persons",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_Email",
+                table: "Persons",
+                column: "Email");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Persons_Email",
+                table: "Persons");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
--- MVCIS/MVCIS/Models/Person.cs
+++ MVCIS/MVCIS/Models/Person.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVCIS.Models
 {
+    [Index("Email")]
     public class Person
     { 
         public int Id { get; set; }
