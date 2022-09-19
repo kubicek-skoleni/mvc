d5504140b4fba800e48cac5c13012c309f3bef54
LukasKubicek <lukaskubicek@gmail.com>
2022-09-19 14:36:15 +0200

    ukladani request logu do databaze

--- asp2/asp2/RazorPages/Middleware/LogRequestMiddleware.cs
+++ asp2/asp2/RazorPages/Middleware/LogRequestMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using System.Threading.Tasks;
@@ -15,11 +16,20 @@ namespace RazorPages.Middleware
             _next = next;
         }
 
-        public Task Invoke(HttpContext httpContext)
+        public Task Invoke(HttpContext httpContext, PersonsDbContext db)
         {
             var ip = httpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
             var url = httpContext.Request.GetDisplayUrl();
             var user_agent = httpContext.Request.Headers["User-Agent"].First();
+
+            db.LogRequests.Add(new Data.Model.LogRequest()
+            {
+                Date = DateTime.Now,
+                Ip = ip,
+                Url = url,
+                UserAgent = user_agent
+            });
+            db.SaveChanges();
 
             return _next(httpContext);
         }
