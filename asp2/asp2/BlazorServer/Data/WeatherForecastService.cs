58206ae741cf33e8bfd8023f620c19396fc08f90
LukasKubicek <lukaskubicek@gmail.com>
2022-09-20 16:28:46 +0200

    blazor server

---
+++ asp2/asp2/BlazorServer/Data/WeatherForecastService.cs
@@ -0,0 +1,20 @@
+namespace BlazorServer.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        public WeatherForecast[] GetForecastAsync(DateTime startDate)
+        {
+            return Enumerable.Range(1, 500).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray();
+        }
+    }
+}
