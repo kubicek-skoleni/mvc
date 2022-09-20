7ca75b7cbfa26b0833e3fa9fcf503668a72192b2
LukasKubicek <lukaskubicek@gmail.com>
2022-09-20 09:48:08 +0200

    controller pro cteni souboru - sync a async

---
+++ asp2/asp2/WEBAPI/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class FileController : ControllerBase
+    {
+
+        [HttpGet("sync")]
+        public ActionResult<int> ReadSync()
+        {
+            var lines = System.IO.File.ReadAllLines(@"C:\skoleni\github\mvc\words01.txt");
+            return lines.Count();
+        }
+        
+        [HttpGet("async")]
+        public async Task<ActionResult<int>> ReadAsync()
+        {
+            var lines = await System.IO.File.ReadAllLinesAsync(@"C:\skoleni\github\mvc\words01.txt");
+            return lines.Count();
+        }
+    }
+}
