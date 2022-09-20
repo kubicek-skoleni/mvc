7f4133755788b67a6111d48f67ecfed5812a5b19
LukasKubicek <lukaskubicek@gmail.com>
2022-09-20 13:57:59 +0200

    json ignore

--- asp2/asp2/Data/Model/Contracts.cs
+++ asp2/asp2/Data/Model/Contracts.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data.Model
